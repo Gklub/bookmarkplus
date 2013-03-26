652f00489f1e23d5f51c07fd9ad14d9c6c0f426f
PortWatcher <root@pwhack.me>
2013-03-26 11:36:08 +0800

    finish combine bookmarks and drag bookmark to folder function..

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,26 +1,31 @@
 /*----------------------------------------
-			codz by PortWatcher
-			2013.3.23 
-			GeeKlub.org
-------------------------------------------
-*/
+*			codz by PortWatcher
+*			2013.3.26 
+*			GeeKlub.org
+*-----------------------------------------*/
+
 
 
 //全局变量，用于存放正在操作的书签id
-var operateID;
+var idOperating1 = 0;
+var idOperating2 = 0;
 
 //初始化，绑定事件监听－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
 $(dumpBookmarks());
+//绑定ondrop和ondragover事件
 $($('#dustbin').on('dragover', function(event) {
 	event.preventDefault();
+	$('#dustbin').addClass("dusbinhover");
 }));
-
 $($('#dustbin').on('drop', function(event) {
 	event.preventDefault();
-	var id = operateID;
+	var id = idOperating1;
 	chrome.bookmarks.remove(id);
-	$('#block').attr('data-id', id).remove();
+	$('[data-id=' + id + ']').remove();
 	$('#dustbin').hide();
+	
+	//clean
+	clean();
 }));
 
 
@@ -95,21 +100,95 @@ function dumpNode(bookmarkNode, query) {
 			'data-id': bookmarkNode.id,
 		});
 		block.text(bookmarkNode.title);
+		
 		//绑定block的点击事件，点击的时候弹出新tab
 		block.click(function() {
 			chrome.tabs.create({url: bookmarkNode.url});
 		});
+		
+		//绑定ondragstart事件
 		block.on('dragstart', function() {
 			$('#dustbin').show();
-			operateID = $(this).attr('data-id');
-			console.log(operateID); //chrome控制台调试
-		}); 
+			idOperating1 = $(this).attr('data-id');
+		});
+		block.on('dragover', function(event) {
+			event.preventDefault();
+		});
+		
+		//绑定书签block的ondrop事件，当两个block重叠时将它们合并到一个书签夹里
+		if(bookmarkNode.url) {
+			//console.log("id to drop:" + block.attr('data-id'));
+			block.on('drop', function(event) {
+				event.preventDefault();
+				handleBookmarkDrop(block.attr('data-id'));
+			});
+		}else {			//绑定书签夹block的ondrop事件，将书签移动到书签夹里
+			block.on('drop', function(event) {
+				event.preventDefault();
+				handleFolderDrop(block.attr('data-id'));
+			});
+		}
 	}
 	if (bookmarkNode.children && bookmarkNode.children.length > 0) {		
 		dumpTreeNodes(bookmarkNode.children, query);
     }
 
 	return block;
+}
+
+//handleBookmarkDrop--------------------------------------------
+function handleBookmarkDrop(idDrop) {
+	var idDropped = idOperating1;
+	if(idDrop != idDropped && idDrop) {
+		console.log("hey, I am in handleBookmarkDrop");
+		
+		//将被拖过来的书签从用户界面上删除
+		$('[data-id=' + idDropped + ']').remove();
+		
+		//创建书签夹，不指定url
+		var bookmarkFolderCreated =  chrome.bookmarks.create({
+			parentId: "1",
+			title: "Bookmark Folder"
+		}, function(bookmarkFolderCreated) {
+			console.log("BookmarkFolder has been created successfully." + bookmarkFolderCreated);
+			//将两个书签都移动到书签夹里
+			chrome.bookmarks.move(
+				idDropped,
+				{parentId: bookmarkFolderCreated.id}
+			);
+			chrome.bookmarks.move(
+				idDrop,
+				{parentId: bookmarkFolderCreated.id}
+			);
+		});
+		
+		//clean
+		clean();
+		
+		location.reload();
+	}
+}
+
+
+//handleFolderDrop----------------------------------------------
+function handleFolderDrop(idDrop) {
+	var idDropped = idOperating1;
+	chrome.bookmarks.move(
+		idDropped,
+		{parentId: idDrop}
+	);
+	
+	//clean
+	clean();
+	
+	location.reload();
+}
+
+
+//clean--------------------------------------------------------------------
+function clean() {
+	idOperating1 = 0;
+	idOperating2 = 0;
 }
 
 
